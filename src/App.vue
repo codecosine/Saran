06ac66780427c77d986fbe48b4bc91fc5140f10c
codecosine <846400886@qq.com>
2017-11-22 14:53:47 +0800

    登录界面。。

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,17 @@
       </el-header>
       <router-view></router-view>
     </el-container>
+    <el-footer class="bd-footer text-muted">
+        <div class="bd-footer-container">
+          <ul class="bd-footer-links">
+            <li><a href="https://github.com/codecosine/Saran">@朝岚日语社团应用</a></li>
+            <li><a href="https://github.com/codecosine/Saran">GitHub</a></li>
+            <li><a href="https://github.com/codecosine/Saran">使用简要</a></li>
+          </ul>
+          <p>技术支持:Element + vue + express</p>
+          <p>Currently v1.0.0-beta. Code licensed <a href="https://github.com/codecosine/Saran/master/LICENSE" target="_blank" rel="license noopener">MIT</a>, docs <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="license noopener"></a>.</p>
+        </div>
+    </el-footer>
   </div>
 </template>
 
--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,53 +1,64 @@
 <template>
   <div>
+    
     <el-container>
         <el-main>
             <div class="content">
-                <h4>注册</h4>
-                <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px">
-                    <el-form-item
-                        prop="mail"
-                        label="邮箱"
-                        :rules="[
-                        { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-                        { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
-                        ]"
-                    >
-                        <el-input v-model="ruleForm2.mail"></el-input>
-                    </el-form-item>
-                     <el-form-item label="姓名" prop="name">
-                        <el-input v-model="ruleForm2.name"></el-input>
-                    </el-form-item>
-                    <el-form-item label="密码" prop="pass">
-                        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
-                    </el-form-item>
-                    <el-form-item label="确认密码" prop="checkPass">
-                        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
-                    </el-form-item>
-                   
-                    <el-form-item>
-                        <el-button type="primary" @click="register">注册</el-button>
-                    </el-form-item>
-                </el-form>
-                <h4>登录</h4>
-                <el-form :model="ruleForm1" status-icon :rules="rules1" ref="ruleForm1" label-width="100px">
-                    <el-form-item
-                        prop="mail"
-                        label="邮箱"
-                        :rules="[
-                        { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-                        { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
-                        ]"
-                    >
-                        <el-input v-model="ruleForm1.mail"></el-input>
-                    </el-form-item>
-                    <el-form-item label="密码" prop="pass">
-                        <el-input type="password" v-model="ruleForm1.pass" auto-complete="off"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="login">登录</el-button>
-                    </el-form-item>
-                </el-form>
+            <el-row :gutter="20">
+                <el-col :span="6" :offset="14">
+                    <h4 class="signTab">
+                        <a :class="{'active': cuurrentIndex == 1 }" @click="changeTab(1)">登录</a>
+                        <a :class="{'active': cuurrentIndex == 2 }" @click="changeTab(2)">注册</a>
+                    </h4>
+                </el-col>
+                <el-col :span="6" :offset="14" v-show="cuurrentIndex == 2">
+                    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px">
+                        <el-form-item
+                            prop="mail"
+                            label="邮箱"
+                            :rules="[
+                            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+                            { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+                            ]"
+                        >
+                            <el-input v-model="ruleForm2.mail"></el-input>
+                        </el-form-item>
+                        <el-form-item label="姓名" prop="name">
+                            <el-input v-model="ruleForm2.name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码" prop="pass">
+                            <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item label="确认密码" prop="checkPass">
+                            <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+                        </el-form-item>
+                    
+                        <el-form-item>
+                            <el-button type="primary" @click="register">注册</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-col>
+                <el-col :span="6" :offset="14" v-show="cuurrentIndex == 1">
+                    <el-form :model="ruleForm1" status-icon :rules="rules1" ref="ruleForm1" label-width="100px">
+                        <el-form-item
+                            prop="mail"
+                            label="邮箱"
+                            :rules="[
+                            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+                            { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+                            ]"
+                        >
+                            <el-input v-model="ruleForm1.mail"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码" prop="pass">
+                            <el-input type="password" v-model="ruleForm1.pass" auto-complete="off"></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="login">登录</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-col>
+            </el-row>
             </div>
         </el-main>
     </el-container>
@@ -87,6 +98,7 @@ export default {
             }
         };
         return {
+            cuurrentIndex: 1,
             ruleForm1: {
                 mail:'',
                 pass: '',
@@ -136,15 +148,31 @@ export default {
                 }
             });
         },
+        changeTab(index){
+            this.cuurrentIndex = index;
+        }
       
     }
 }
 </script>
 <style lang="less" scoped>
 .content{
-    padding: 10px 10px 0px 40px;
-    h1{
-        color: #666;
+    //background: url('../assets/gdut.jpeg') no-repeat;
+    // background-size:100% 100%;
+    background-origin:border-box;
+
+}
+.signTab{
+    padding-left: 50px;
+    .active {
+         color: #399faa
+    }
+    a{
+        padding:10px;
+        cursor: pointer;
+        &:hover{
+            color: #399faa
+        }
     }
 }
 
